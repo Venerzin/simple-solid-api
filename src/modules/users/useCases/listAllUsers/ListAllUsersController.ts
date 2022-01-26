971d5b3aae9a170a1ce63282a2cb807ebca1f735
Venerzin <rozaviniciusdeveloper@gmail.com>
2022-01-26 20:24:59 -0300

    Learning SOLID initial concepts

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { user_id } = request.headers;
+
+    if (typeof user_id !== "string") {
+      return response.status(404).send();
+    }
+
+    try {
+      const user = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(user);
+    } catch (message) {
+      return response.status(400).json({ error: message });
+    }
+  }
+}
+
+export { ListAllUsersController };
